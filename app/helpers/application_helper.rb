9e35fe6d83623b44983d111c98d29b5a30b1fb2a
Goulven Champenois <goulven.champenois@userland.fr>
2025-02-12 11:24:02 +0100

    Add magic page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  # Automagically fetch title from @title, content_for(:title), resource.to_title, or controller/action/title I18n lookup
+  def page_title
+    return @title unless @title.blank?
+    return content_for(:title) if content_for?(:title)
+    return resource.to_title if action_name == "show"
+
+    action = case action_name.to_sym
+    when :create then :new
+    when :update then :edit
+    else action_name
+    end
+    t("#{controller_name}.#{action}.title")
+  end
+
+  def head_title
+    if root?
+      [t("global.service_name"), t("global.service_description")].compact_blank.join(" : ")
+    else
+      [page_title, t("global.service_name")].compact_blank.join(" - ")
+    end
+  end
+
+  def root? = request.path == "/"
 end
