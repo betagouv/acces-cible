75927da29e5b2d10c723eaffb0c1f17f5498b572
Goulven Champenois <goulven.champenois@userland.fr>
2025-02-12 12:01:50 +0100

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  scope controller: :pages do
+    root action: :accueil
+    get "accessibilite", as: :accessibilite
+    get "plan", as: :plan
+    get "contact", as: :contact
+    get "mentions-legales", as: :mentions_legales
+    get "cookies", as: :cookies
+    get "donnees-personnelles", as: :donnees_personnelles
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
