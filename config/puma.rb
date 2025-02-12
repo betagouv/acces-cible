0026f80356b30b60d5fed2667fc4392eea9bb6fa
Goulven Champenois <goulven.champenois@userland.fr>
2025-02-12 16:02:50 +0100

    Update Puma config for Scalingo

--- config/puma.rb
+++ config/puma.rb
@@ -24,17 +24,25 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+workers Integer(ENV["WEB_CONCURRENCY"] || 2)
+threads_count = Integer(ENV["MAX_THREADS"] || 5)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+preload_app!
+
+port        ENV["PORT"]     || 3000
+environment ENV["RACK_ENV"] || "development"
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  ActiveRecord::Base.establish_connection
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || Rails.env.development?
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
