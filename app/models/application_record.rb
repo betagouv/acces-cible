6217ed0c63e6ae03e462756f679b148709bb3469
Goulven Champenois <goulven.champenois@userland.fr>
2025-02-13 14:40:09 +0100

    Add basic translation helpers

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  delegate :l, to: :I18n
+  delegate :human, :human_count, to: :class
+
+  class << self
+    alias human human_attribute_name
+    def human_count(count, attr = nil) = human(attr || :count, count:)
+  end
 end
