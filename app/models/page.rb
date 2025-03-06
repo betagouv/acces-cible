5a8db5a77009449aee00689a022ce6af826ed135
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-06 14:03:45 +0100

    Remove unneeded require net::http in page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,3 @@
-require "net/http"
-
 class Page < Data.define(:url, :root)
   CACHE_TTL = 10.minutes
   SKIPPED_EXTENSIONS = /\.(xml|rss|atom|pdf|zip|doc|docx|xls|xlsx|ppt|pptx|jpg|jpeg|png|gif|mp3|mp4|avi|mov)$/i
