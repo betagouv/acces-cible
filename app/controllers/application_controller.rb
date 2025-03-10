01c0dd6d76cb7f865c906f54e5dbf8d9240897de
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-10 11:32:31 +0100

    fix: Fix crash when displaying an error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,17 @@ class ApplicationController < ActionController::Base
   helper_method :resource, :resource_model
 
   rescue_from ActionController::RoutingError, ActiveRecord::RecordNotFound, ActiveStorage::FileNotFoundError do
+    @title = t("errors.not_found.title")
     respond_to do |format|
       format.any  { head :not_found }
-      format.html { render "errors#not_found", status: :not_found }
+      format.html { render "errors/not_found", status: :not_found }
     end
   end
   rescue_from ActiveRecord::NotNullViolation do |exception|
+    @title = t("errors.internal_server_error.title")
     respond_to do |format|
       format.any  { head :internal_server_error }
-      format.html { render "errors#internal_server_error", status: :internal_server_error }
+      format.html { render "errors/internal_server_error", status: :internal_server_error }
     end
   end
 
