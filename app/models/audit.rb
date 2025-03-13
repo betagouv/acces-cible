288cc81284ae30a06e167962ddca6b7d0464215d
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-13 11:35:30 +0100

    Simplify accessing existing audit checks

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -24,6 +24,7 @@ class Audit < ApplicationRecord
 
   def parsed_url = @parsed_url ||= URI.parse(url).normalize
   def url_without_scheme = @url_without_scheme ||= [hostname, path == "/" ? nil : path].compact.join(nil)
+  def checks = Check.find_by(audit: self)
 
   def all_checks
     Check.names.map { |name| send(name) || send(:"build_#{name}") }
