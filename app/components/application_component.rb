1de39b7ce776b97ab3efaba0e0405da801ef18fc
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-18 21:57:03 +0100

    Add human helper to application components

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationComponent < ViewComponent::Base
+  delegate :human, to: :class
+
+  class << self
+    def human(key, options = {})
+      options[:count] ||= 1
+      component = name.underscore.gsub("/", ".").delete_suffix("_component")
+      I18n.translate("viewcomponent.#{component}.#{key}", **options)
+    end
+  end
 end
