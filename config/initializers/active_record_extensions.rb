c9ec9bdfa3292a5a0c2baf04aef684e733e74098
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-21 09:58:12 +0100

    refactor: Improve sort params extraction readability

--- config/initializers/active_record_extensions.rb
+++ config/initializers/active_record_extensions.rb
@@ -11,9 +11,7 @@ ActiveSupport.on_load(:active_record) do
     end
 
     def sort_by(params, default_scope: nil)
-      key, direction = params[:sort]&.to_unsafe_h&.first
-      directions = [:asc, :desc]
-      direction = direction.to_s.downcase.to_sym.presence_in(directions) || directions.first
+      key, direction = extract_sort_from(params)
       if query_object_class
         query_object_class.new(self).sort_by(key&.to_sym, direction:, default_scope:) || self
       elsif key = key.presence_in(model.column_names)
@@ -23,6 +21,13 @@ ActiveSupport.on_load(:active_record) do
       else
         order("created_at" => direction)
       end
+    end
+
+    def extract_sort_from(params)
+      directions = [:asc, :desc]
+      key, direction = params[:sort]&.to_unsafe_h&.first
+      direction = direction.to_s.downcase.to_sym.presence_in(directions) || directions.first
+      [key, direction]
     end
   end
   ActiveRecord::Relation.include(QueryExtensions)
