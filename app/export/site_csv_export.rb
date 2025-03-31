223531974a6348cf0ae509bbcf29ba0da1abed70
Goulven Champenois <goulven.champenois@userland.fr>
2025-03-31 12:34:10 +0200

    feat: Export human-readable percentages

--- app/export/site_csv_export.rb
+++ app/export/site_csv_export.rb
@@ -18,10 +18,10 @@ class SiteCsvExport < ApplicationExport
       Checks::LanguageIndication.human(:type) => [:audit, :language_indication, :indication],
       Checks::AccessibilityMention.human(:type) => [:audit, :accessibility_mention, :mention_text],
       Checks::FindAccessibilityPage.human(:type) => [:audit, :find_accessibility_page, :url],
-      Checks::AnalyzeAccessibilityPage.human(:compliance_rate) => [:audit, :analyze_accessibility_page, :compliance_rate],
+      Checks::AnalyzeAccessibilityPage.human(:compliance_rate) => [:audit, :analyze_accessibility_page, :human_compliance_rate],
       Checks::AnalyzeAccessibilityPage.human(:audit_date) => [:audit, :analyze_accessibility_page, :audit_date],
       Checks::AnalyzeAccessibilityPage.human(:audit_update_date) => [:audit, :analyze_accessibility_page, :audit_update_date],
-      Checks::RunAxeOnHomepage.human(:success_rate) => [:audit, :run_axe_on_homepage, :success_rate],
+      Checks::RunAxeOnHomepage.human(:success_rate) => [:audit, :run_axe_on_homepage, :human_success_rate],
     }
   end
 
