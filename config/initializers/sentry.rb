adba45ecfacb89cc9da07a0c9308886629eb51f1
Goulven Champenois <goulven.champenois@userland.fr>
2025-04-14 15:10:50 +0200

    ops: Ignore server_name in Sentry to prevent duplicate errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,12 @@ Sentry.init do |config|
   config.dsn = "https://cb48a87a9a7c2f33bf362a8d91c0a594@sentry.incubateur.net/218"
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
+  config.before_send = lambda do |event, hint|
+    # Remove server_name from the event so it doesn't affect grouping
+    event.server_name = nil
+    event
+  end
+
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
