c0a2c57acdce0dec3cdea5952f2a4343627b16d3
Goulven Champenois <goulven.champenois@userland.fr>
2025-04-18 10:45:17 +0200

    refactor: Simplify using helpers in ApplicationRecord models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,10 +2,11 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   delegate :l, to: :I18n
-  delegate :human, :human_count, to: :class
-  delegate :helpers, to: ApplicationController
+  delegate :human, :human_count, :helpers, to: :class
 
   class << self
+    delegate :helpers, to: ApplicationController
+
     alias human human_attribute_name
     def human_count(attr = :count, count: nil) = human(attr, count: count || send(attr))
   end
