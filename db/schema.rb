893d99dea9a40ed05b7342159c59425e61006b7a
Goulven Champenois <goulven.champenois@userland.fr>
2025-04-26 23:20:06 +0200

    Remove attempts column from Audit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_06_090321) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_26_211016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,12 +18,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_06_090321) do
     t.bigint "site_id", null: false
     t.string "url", null: false
     t.string "status", null: false
-    t.integer "attempts", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "checked_at"
     t.index "regexp_replace((url)::text, '^https?://(www.)?'::text, ''::text)", name: "index_audits_on_normalized_url"
-    t.index ["attempts"], name: "index_audits_on_retryable", where: "(((status)::text = 'failed'::text) AND (attempts > 0))"
     t.index ["site_id"], name: "index_audits_on_site_id"
     t.index ["url"], name: "index_audits_on_url"
   end
