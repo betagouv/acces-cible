99fbfa9c86e347d61cfe57a3fd7feeca1e461b85
Goulven Champenois <goulven.champenois@userland.fr>
2025-05-12 15:12:46 +0200

    test: Set Faker locale, but don't crash when it falls back to English

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -5,4 +5,9 @@ Rails.application.configure do
   config.i18n.default_locale = :fr
   config.time_zone = "Europe/Paris"
   config.i18n.raise_on_missing_translations = Rails.env.local?
+
+  # Workaround until there's a fix for https://github.com/faker-ruby/faker/issues/2987
+  # TL;DR Faker falls back to :en when the current locale doesn't contain a key,
+  # but this fails because :en isn't included in config.i18n.available_locales.
+  config.i18n.available_locales << :en if Rails.env.test?
 end
