bf34d5cb55f2e40ce39673644c97908188659933
Goulven Champenois <goulven.champenois@userland.fr>
2025-05-16 15:11:23 +0200

    perf: Adjust solid queue retention period and cleanup schedule

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,4 +1,4 @@
 Rails.application.config.solid_queue.configure do |config|
-  config.cleanup_interval = 1.day
-  config.delete_finished_jobs_after = 1.month
+  config.delete_finished_jobs_after = 10.days
+  config.cleanup_interval = 6.hours
 end
