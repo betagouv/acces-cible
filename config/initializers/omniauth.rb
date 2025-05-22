f90eb9da54db8cc49a385e9105c6141dc5e9396d
Goulven Champenois <goulven.champenois@userland.fr>
2025-05-22 18:33:20 +0200

    Update credentials to simplify proconnect strategy setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [:uid], uid_field: :uid if Rails.env.local?
-    proconnect = Rails.application.credentials.proconnect[Rails.application.staging? ? :staging : Rails.env]
 
-    provider :proconnect,
-      {
-        client_id: proconnect.client_id,
-        client_secret: proconnect.client_secret,
-        proconnect_domain: proconnect.host,
-        redirect_uri: proconnect.redirect_uri,
-        post_logout_redirect_uri: proconnect.post_logout_redirect_uri,
-        scope: "openid email given_name usual_name siret organizational_unit belonging_population"
-      }
+  scope = "openid email given_name usual_name siret organizational_unit belonging_population"
+  provider :proconnect, Rails.application.credentials.proconnect[Rails.env].to_h.merge(scope:)
 end
 
 # Make Omniauth compatible with rack_csrf
