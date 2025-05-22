dacd819acab3315c6416d11f0568a227513001f9
Goulven Champenois <goulven.champenois@userland.fr>
2025-05-23 00:33:49 +0200

    fix: Fetch staging proconnect options in staging app

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [:uid], uid_field: :uid if Rails.env.local?
 
   scope = "openid email given_name usual_name siret organizational_unit belonging_population"
-  provider :proconnect, Rails.application.credentials.proconnect[Rails.env].to_h.merge(scope:)
+  proconnect_options = Rails.application.credentials.proconnect[Rails.application.staging? ? :staging : Rails.env]
+  provider :proconnect, proconnect_options.to_h.merge(scope:)
 end
 
 # Make Omniauth compatible with rack_csrf
