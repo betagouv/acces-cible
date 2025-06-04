1490ac93fc3394fce62f1754d83b6530bebc9c02
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-04 16:57:04 +0200

    feat: Remove unused tags after 1 day

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -10,6 +10,8 @@ class Tag < ApplicationRecord
   validates :name, presence: true, uniqueness: { scope: :team_id }, if: :name_changed?
 
   scope :in_alphabetical_order, -> { order(:name) }
+  scope :orphaned, -> { where.missing(:site_tags) }
+  scope :not_recently_used, -> { where(updated_at: ..1.day.ago) }
 
   def to_s = name
 
