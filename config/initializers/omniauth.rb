e521a05b2443c53ebb104340ffa4ab2e9b4ea37f
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-16 10:06:52 +0200

    ci: Fix CI config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [:uid], uid_field: :uid if Rails.env.local?
 
   scope = "openid email given_name usual_name siret organizational_unit belonging_population"
+
   proconnect_options = Rails.application.credentials.proconnect[Rails.application.staging? ? :staging : Rails.env]
   provider :proconnect, proconnect_options.to_h.merge(scope:)
 end
