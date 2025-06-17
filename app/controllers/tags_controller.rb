5c4257cd6259a95905e52ef501b61315146cd389
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-17 22:57:19 +0200

    refactor: Inline create action tag params for clarity

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,10 @@
 class TagsController < ApplicationController
   # POST /tags
   def create
-    new_tag = current_user.team.tags.find_or_create_by(name: tag_params[:tags_attributes][:name])
+    create_params = params.require(upload? ? :site_upload : :site)
+    new_tag = current_user.team.tags.find_or_create_by(name: create_params[:tags_attributes][:name])
     if new_tag.persisted?
-      tag_ids = (tag_params[:tag_ids] || []).push(new_tag.id).compact
+      tag_ids = (create_params[:tag_ids] || []).push(new_tag.id).compact
       object = template_object_klass.new(tag_ids:, team: current_user.team)
       render turbo_stream: turbo_stream.replace(:site_tags, partial: "sites/tags_form", locals: { object:, focus: true })
     else
@@ -15,5 +16,4 @@ class TagsController < ApplicationController
 
   def upload? = params.key?(:site_upload)
   def template_object_klass = upload? ? SiteUpload : Site
-  def tag_params = params.require(upload? ? :site_upload : :site)
 end
