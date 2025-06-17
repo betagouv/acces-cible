e34494ed8707bef941d1fe75a7da11a17590bec2
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-17 15:46:26 +0200

    feat: Add sites_count to tags

--- app/models/site_tag.rb
+++ app/models/site_tag.rb
@@ -1,4 +1,4 @@
 class SiteTag < ApplicationRecord
-  belongs_to :site
-  belongs_to :tag, touch: true
+  belongs_to :site, counter_cache: :tags_count
+  belongs_to :tag, counter_cache: :sites_count, touch: true
 end
