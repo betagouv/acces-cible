9ae3d50dcb0397cb8fed3d39706e22f6e2babb78
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-23 10:13:41 +0200

    ci: Skip proconnect setup in CI
    Credentials fail to load

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [:uid], uid_field: :uid if Rails.env.local?
 
-  scope = "openid email given_name usual_name siret organizational_unit belonging_population"
-
-  proconnect_options = Rails.application.credentials.proconnect[Rails.application.staging? ? :staging : Rails.env]
-  provider :proconnect, proconnect_options.to_h.merge(scope:)
+  unless Rails.env.test?
+    scope = "openid email given_name usual_name siret organizational_unit belonging_population"
+    proconnect_options = Rails.application.credentials.proconnect[Rails.application.staging? ? :staging : Rails.env]
+    provider :proconnect, proconnect_options.to_h.merge(scope:)
+  end
 end
 
 # Make Omniauth compatible with rack_csrf
