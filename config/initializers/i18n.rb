5148d8a16df8fcc9da717090765a919c8c07b007
Goulven Champenois <goulven.champenois@userland.fr>
2025-06-30 14:59:16 +0200

    config: Allow using Faker in dev console without errors

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,12 +1,14 @@
-# frozen_string_literal: true
-
 Rails.application.configure do
   config.i18n.available_locales = [:fr]
-  config.i18n.default_locale = :fr
+  config.i18n.default_locale = config.i18n.available_locales.first
   config.i18n.raise_on_missing_translations = Rails.env.local?
 
-  # Workaround until there's a fix for https://github.com/faker-ruby/faker/issues/2987
-  # TL;DR Faker falls back to :en when the current locale doesn't contain a key,
-  # but this fails because :en isn't included in config.i18n.available_locales.
-  config.i18n.available_locales << :en if Rails.env.test?
+  if Rails.env.local? && defined?(Faker)
+    # Setup Faker so that it works both in test and dev console
+    Faker::Config.locale = config.i18n.default_locale
+
+    # Workaround for https://github.com/faker-ruby/faker/issues/2987
+    # TL;DR Faker I18n falls back to :en, which raises when available_locales doesn't include :en.
+    config.i18n.enforce_available_locales = false
+  end
 end
