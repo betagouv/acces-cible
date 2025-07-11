f1f08e1f4a83a54bf019cf433734e8e50cb3ce0d
Goulven Champenois <goulven.champenois@userland.fr>
2025-07-11 12:55:02 +0200

    fix: Ignore duplicates when importing sites

--- app/models/site_upload.rb
+++ app/models/site_upload.rb
@@ -27,8 +27,8 @@ class SiteUpload
   def initialize(attributes = {})
     super
     @tag_ids ||= []
-    @new_sites = []
-    @existing_sites = []
+    @new_sites = {}
+    @existing_sites = {}
   end
 
   def save
@@ -37,8 +37,8 @@ class SiteUpload
     parse_sites
 
     transaction do
-      create!(new_sites) if new_sites.any?
-      existing_sites.each { |site| site.save && site.audit! }
+      create!(new_sites.values) if new_sites.any?
+      existing_sites.values.each { |site| site.save && site.audit! }
     end
     true
   end
@@ -71,12 +71,11 @@ class SiteUpload
       if existing_site = team.sites.find_by_url(url:)
         existing_site.assign_attributes(tag_ids: tag_ids.union(existing_site.tag_ids))
         existing_site.assign_attributes(name:) unless existing_site.name
-        self.existing_sites << existing_site
+        self.existing_sites[url] = existing_site
       else
-        self.new_sites << { url:, team:, name:, tag_ids: }
+        self.new_sites[url] = { url:, team:, name:, tag_ids: }
       end
     end
-    self.new_sites.uniq!
   end
 
   private
