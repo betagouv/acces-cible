15825f5edc2e8107ec1f4b36973665bd80fe956e
Manon Budin <manon.budin@beta.gouv.fr>
2025-07-15 10:15:27 +0200

    chore: clean up commented line

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
   def omniauth
     if user = User.from_omniauth(request.env["omniauth.auth"])
       start_new_session_for user
-      # flash[:notice] = Session.human(:login_success)
       redirect_to after_authentication_url, notice: t(".success")
     else
       redirect_to auth_failure_path
