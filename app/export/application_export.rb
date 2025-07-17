5e838951dbbdfba1ba539b7f5fe44c3def2ec95f
Goulven Champenois <goulven.champenois@userland.fr>
2025-07-17 13:16:59 +0200

    refactor: Extract common Export code

--- app/export/application_export.rb
+++ app/export/application_export.rb
@@ -9,7 +9,24 @@ class ApplicationExport
     @relation = relation
   end
 
-  def extension = self.class::EXTENSION
+  def extension
+    self.class::EXTENSION
+  rescue NameError
+    raise NotImplementedError, "#{self.class} must define EXTENSION constant"
+  end
+
+  def attributes
+    raise NotImplementedError, "#{self.class} must implement #attributes method"
+  end
+
   def filename = "#{table_name}_#{l Time.zone.now, format: :file}.#{extension}"
   def records = relation.find_each
+  def headers = attributes.keys
+
+  def serialize(record)
+    attributes.values.map do |methods|
+      # Turns [:a, :b, :c] into record.a&.b&.c
+      Array.wrap(methods).reduce(record) { |obj, method| obj&.public_send(method) }
+    end
+  end
 end
