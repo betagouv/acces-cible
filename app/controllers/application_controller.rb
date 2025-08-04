fa3b138a03b36a0c11bfb930e359183f73c6c5f8
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-04 16:48:28 +0200

    feat: Report omniauth failures to Sentry

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   include Authentication
+  include ErrorHelper
   include ActionView::RecordIdentifier
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
