790fc9d3c743cb20381fe1c235b153a154090b61
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-04 17:24:30 +0200

    feat: Shorten delay between ProcessAuditJob reschedules

--- app/jobs/process_audit_job.rb
+++ app/jobs/process_audit_job.rb
@@ -31,7 +31,7 @@ class ProcessAuditJob < ApplicationJob
     next_retry_at = checks.failed.retryable.minimum(:retry_at)
     next_run_at = checks.pending.minimum(:run_at)
 
-    wait_until = [next_retry_at, next_run_at].compact.min || 5.minutes.from_now
+    wait_until = [next_retry_at, next_run_at].compact.min || 1.minute.from_now
     self.class.set(wait_until:, group: "audit_#{audit.id}").perform_later(audit)
   end
 end
