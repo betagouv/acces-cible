e49d53cb4a075fc4fe39c602019a020d50b21dfe
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-05 17:27:28 +0200

    feat: Allow English locale as fallback because Faker and MissionControl require it

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,14 +1,5 @@
 Rails.application.configure do
-  config.i18n.available_locales = [:fr]
+  config.i18n.available_locales = [:fr, :en]
   config.i18n.default_locale = config.i18n.available_locales.first
   config.i18n.raise_on_missing_translations = Rails.env.local?
-
-  if Rails.env.local? && defined?(Faker)
-    # Setup Faker so that it works both in test and dev console
-    Faker::Config.locale = config.i18n.default_locale
-
-    # Workaround for https://github.com/faker-ruby/faker/issues/2987
-    # TL;DR Faker I18n falls back to :en, which raises when available_locales doesn't include :en.
-    config.i18n.enforce_available_locales = false
-  end
 end
