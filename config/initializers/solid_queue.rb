10349c06a2db8daad17b9099c24e59d138a2f811
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-07 17:35:55 +0200

    chore: Purge finished jobs after 2 days instead of 10
    Because of https://github.com/rails/mission_control-jobs/issues/160

--- config/initializers/solid_queue.rb
+++ config/initializers/solid_queue.rb
@@ -1,4 +1,4 @@
 Rails.application.config.solid_queue.configure do |config|
-  config.delete_finished_jobs_after = 10.days
+  config.delete_finished_jobs_after = 2.days
   config.cleanup_interval = 6.hours
 end
