75b6e5747d3df76e78901ebf88f7a882ecd29eb8
Goulven Champenois <goulven.champenois@userland.fr>
2025-08-07 16:44:10 +0200

    fix: Prevent error if the sites.audit_id column doesn't exist
    Because it hasn't been deployed everywhere apparently.

--- db/migrate/20250807100904_remove_audit_id_from_sites.rb
+++ db/migrate/20250807100904_remove_audit_id_from_sites.rb
@@ -1,5 +1,7 @@
 class RemoveAuditIdFromSites < ActiveRecord::Migration[8.0]
-  def change
+  def up
+    return unless column_exists?(:sites, :audit)
+
     remove_reference :sites, :audit, foreign_key: foreign_key_exists?(:sites, :audit)
   end
 end
