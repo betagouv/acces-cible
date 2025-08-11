c3304e56b375db5e8025f9c8f2c96f0c52de6469
Manon Budin <manon.budin@beta.gouv.fr>
2025-08-11 16:31:49 +0200

    chore: omniauth initialize with usual and given name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, fields: [
     :uid,
     :email,
-    :name,
+    :given_name,
+    :usual_name,
     :siret,
     :organizational_unit
   ] if Rails.env.local?
 
-  scope = "openid email name siret organizational_unit belonging_population"
+  scope = "openid email given_name usual_name siret organizational_unit belonging_population"
 
   proconnect_options = Rails.application.credentials.dig(:proconnect, Rails.application.staging? ? :staging : Rails.env)
   provider :proconnect, proconnect_options.to_h.merge(scope:)
