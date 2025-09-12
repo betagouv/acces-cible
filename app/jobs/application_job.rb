c8f9c2b08ab8f2720c2c1cca32cc45a36a99146d
Goulven Champenois <goulven.champenois@userland.fr>
2025-09-12 10:34:48 +0200

    feat: Monitor jobs using Sentry

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -7,4 +7,27 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError, ActiveRecord::RecordNotFound
+
+  around_perform :monitor_with_sentry
+
+  private
+
+  def monitor_with_sentry
+    Sentry.with_transaction(
+      op: "queue.solid_queue",
+      name: self.class.name
+    ) do |transaction|
+      transaction.set_data(:queue, queue_name)
+      transaction.set_data(:arguments, arguments)
+
+      yield
+    end
+  rescue => error
+    Sentry.capture_exception(error, extra: {
+      job_class: self.class.name,
+      arguments: arguments,
+      queue: queue_name
+    })
+    raise
+  end
 end
