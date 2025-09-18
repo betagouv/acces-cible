51efefb4b1b613a96a23ffe729256e5131808c5f
Manon Budin <manon.budin@beta.gouv.fr>
2025-09-18 17:02:57 +0200

    feat: Add tag filtering to site index
    - Update `SiteQuery` to support filtering by tag using `tag_id`
    - Add a tag filter dropdown to the site index view
    - Load tags in `SitesController` for filtering options
    - Remove 'N/A' when no tags are present in the index

--- features/step_definitions/site_steps.rb
+++ features/step_definitions/site_steps.rb
@@ -3,3 +3,10 @@
 Quand("je possède un fichier {string} qui contient") do |path, content|
   File.write(path, content)
 end
+
+Quand("je filtre par étiquette {string}") do |tag|
+  steps %(
+    Quand je sélectionne "#{tag}" pour "Filtrer par étiquette"
+    Et que je clique sur "Filtrer"
+  )
+end
