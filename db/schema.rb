c12fec4e91815b38f25d9ae1257e8d55925a2d07
Manon Budin <manon.budin@beta.gouv.fr>
2025-12-01 17:50:59 +0100

    feat: Add accessibility_page_url column to audits
    Signed-off-by: Manon Budin <manon.budin@beta.gouv.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_20_092907) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_01_152348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_20_092907) do
     t.boolean "current", default: false, null: false
     t.text "home_page_html"
     t.text "accessibility_page_html"
+    t.string "accessibility_page_url"
     t.index "regexp_replace((url)::text, '^https?://(www.)?'::text, ''::text)", name: "index_audits_on_normalized_url"
     t.index ["site_id", "current"], name: "index_audits_on_site_id_and_current", unique: true, where: "(current = true)"
     t.index ["site_id"], name: "index_audits_on_site_id"
