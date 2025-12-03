aa5ab86a2c84861bbd89ef558d74bae5528fa2d6
Manon Budin <manon.budin@beta.gouv.fr>
2025-12-03 12:41:00 +0100

    chore: Remove retry_on for ActiveRecord::Deadlocked in ApplicationJob
    - This line is muted by default
    - It could be the cause of endlessly running jobs
    
    Signed-off-by: Manon Budin <manon.budin@beta.gouv.fr>

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,8 +3,6 @@ class ApplicationJob < ActiveJob::Base
 
   queue_as :background
 
-  retry_on ActiveRecord::Deadlocked
-
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError, ActiveRecord::RecordNotFound
 
