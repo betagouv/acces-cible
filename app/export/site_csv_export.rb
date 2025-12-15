93853c446338f8911244fb2fa47d6db33a4c094c
Manon Budin <manon.budin@beta.gouv.fr>
2025-12-15 10:55:39 +0100

    feat: Add columns to csv export

--- app/export/site_csv_export.rb
+++ app/export/site_csv_export.rb
@@ -4,17 +4,21 @@ class SiteCsvExport
   HEADERS = [
     Audit.human(:site_url_address),
     Audit.human(:url),
+    Audit.human(:redirected_url),
     Tag.human(:all),
     Check.human(:checked_at),
     Checks::Reachable.human(:type),
     Checks::LanguageIndication.human(:type),
     Checks::AccessibilityMention.human(:type),
     Checks::FindAccessibilityPage.human(:type),
+    Checks::AnalyzeAccessibilityPage.human(:auditor),
     Checks::AnalyzeAccessibilityPage.human(:compliance_rate),
     Checks::AnalyzeAccessibilityPage.human(:audit_date),
     Checks::AnalyzeAccessibilityPage.human(:audit_update_date),
     Checks::AnalyzeSchema.human(:type),
+    Checks::AnalyzeSchema.human(:years),
     Checks::AnalyzePlan.human(:type),
+    Checks::AnalyzePlan.human(:years),
     Checks::AccessibilityPageHeading.human(:type),
     Checks::RunAxeOnHomepage.human(:success_rate),
   ].freeze
@@ -26,7 +30,7 @@ class SiteCsvExport
   def self.stream_csv_to(output_stream, sites)
     output_stream.write CSV.generate_line(HEADERS, col_sep: COL_SEP)
 
-    sites.find_each(batch_size: 500) do |site|
+    sites.each do |site|
       audit = site.audit
 
       reachable = audit&.reachable
@@ -42,17 +46,21 @@ class SiteCsvExport
       row = [
         site.url_without_scheme_and_www,
         site.url,
+        audit.reachable.redirect_url,
         site.tags_list,
         audit&.checked_at,
         reachable&.completed?.to_s,
         extract_value(language, language&.indication),
         extract_value(mention, mention&.mention_text),
         extract_value(find_accessibility, find_accessibility&.url),
+        extract_value(analyze_accessibility, analyze_accessibility&.auditor),
         extract_value(analyze_accessibility, analyze_accessibility&.human_compliance_rate),
         extract_value(analyze_accessibility, analyze_accessibility&.audit_date),
         extract_value(analyze_accessibility, analyze_accessibility&.audit_update_date),
         extract_value(schema, link_or_found(schema, "analyze_schema.schema_in_main_text")),
+        extract_value(schema, schema&.years&.join("-")),
         extract_value(plan, link_or_found(plan, "analyze_plan.plan_in_main_text")),
+        extract_value(plan, plan&.years&.join("-")),
         extract_value(heading, heading&.human_success_rate),
         extract_value(axe, axe&.human_success_rate),
       ]
