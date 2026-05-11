c541d158d09e093aeb713d7c9daf20cfbbb49c04
Manon <manon.budin@beta.gouv.fr>
2026-05-11 11:51:01 +0200

    chore: Add migration to backfill `site` URLs (#553) (#555)
    - Populate `url` and `normalized_url` in `site` using `audit` `url`.
    
    Signed-off-by: Manon Budin <manon.budin@beta.gouv.fr>

---
+++ db/migrate/20260420142312_backfill_site_urls.rb
@@ -0,0 +1,25 @@
+class BackfillSiteUrls < ActiveRecord::Migration[8.1]
+  def up
+    Site.includes(:audits).find_in_batches do |sites|
+      sites.each do |site|
+        audit_url = site.audits.first&.url
+        next if audit_url.blank?
+        normalized_url = Link.url_without_scheme_and_www(audit_url)
+
+        # update_columns here is needed because of an overriding url setter method in Site model
+        site.update_columns(url: audit_url, normalized_url:, updated_at: Time.zone.now)
+      end
+    end
+  end
+
+  def down
+    Audit.includes(:site).find_in_batches do |audits|
+      audits.each do |audit|
+        site_url = audit.site&.url
+        next if site_url.blank? || audit.url.present?
+
+        audit.update(url: site_url)
+      end
+    end
+  end
+end
