8cec36af91d4f4451b84b3c1e41019192d408a38
Yann Petitjean <yann120@hotmail.fr>
2026-05-13 16:47:26 +0200

    chore: Nettoyage du code mort et des dépendances inutilisées (#542)

--- app/models/check.rb
+++ app/models/check.rb
@@ -57,7 +57,7 @@ class Check < ApplicationRecord
 
   scope :prioritized, -> { order(:priority) }
   scope :remaining, -> { in_state(:pending, :blocked) }
-  scope :errored, -> { in_state(:failed) }
+  scope :failed, -> { in_state(:failed) }
 
   class << self
     def human_type
