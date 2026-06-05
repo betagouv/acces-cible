4ceb91aedd0371aa56b0fd68b1f5a02a58d38c2c
Yann Petitjean <yann120@hotmail.fr>
2026-06-05 10:33:35 +0200

    bug: éviter les doublons de tags lors de l’import CSV (#577)

--- app/services/site_batch_creation_service.rb
+++ app/services/site_batch_creation_service.rb
@@ -5,14 +5,14 @@ class SiteBatchCreationService
   end
 
   def process(site_data)
-    site_tag_ids = @tag_ids + tag_ids_from_names(site_data["tag_names"] || [])
+    tag_ids = site_tag_ids(site_data)
     site = @team.sites.find_by(url: site_data["url"])
 
     if site
-      update_site(site, site_data, site_tag_ids)
+      update_site(site, site_data, tag_ids)
       site.audit!
     else
-      Site.create!(url: site_data["url"], team: @team, name: site_data["name"], tag_ids: site_tag_ids.uniq).audit!
+      Site.create!(url: site_data["url"], team: @team, name: site_data["name"], tag_ids:).audit!
     end
   end
 
@@ -22,6 +22,11 @@ class SiteBatchCreationService
     site.tag_ids = site_tag_ids.union(site.tag_ids)
     site.name = site_data["name"] if site_data["name"].present? && site.name.blank?
     site.save!
+  end
+
+  def site_tag_ids(site_data)
+    tag_ids = @tag_ids + tag_ids_from_names(site_data["tag_names"] || [])
+    tag_ids.compact_blank.map(&:to_i).uniq
   end
 
   def tag_ids_from_names(tag_names)
