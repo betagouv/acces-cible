fa573fe1b4a819dbcb4a3f08f0b48b53f830302d
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2026-06-09 16:49:07 +0200

    chrome: use a dedicated Chrome instance
    We've had a long time problem on this project: the complete darkness
    surrounding the crawling done by Chrome within our processes. That's
    because we don't provide a browser URL to Ferrum, and so Ferrum boots
    its own Chrome process within its Ruby process[1].
    
    This means we're unable to correctly debug our jobs containers on
    Scalingo (we can't SH into our running containers, we cannot run `ps`
    on them), and that these containers inevitably grow very large because
    on top of their own overhead (our Ruby/Rails app) they have to
    allocate and run their own Chrome process which adds a fair amount of
    memory and CPU to their own.
    
    Demonstrate a new approach here where we run Chrome separately, on a
    new Docker container we control (on Scalingo we can use a buildpack
    instead[2]) and then we just feed to Ferrum our local URL.
    
    This allows us to log, inspect and monitor our Chrome business while
    keeping our Ruby job containers happy & safe, away from the
    nightmarish world of web browsers.
    
    [1]: https://github.com/rubycdp/ferrum/blob/9fae889c724ab4388bee2c5f54a440e0e095b86a/lib/ferrum/browser.rb#L114-L116
    [2]: https://github.com/betterplace/scalingo-buildpack-google-chrome

--- app/models/browser.rb
+++ app/models/browser.rb
@@ -79,6 +79,7 @@ class Browser
 
     def settings
       {
+        url: ENV.fetch("CHROME_URL"),
         headless: :new,
         timeout: PAGE_TIMEOUT,
         process_timeout: PROCESS_TIMEOUT,
