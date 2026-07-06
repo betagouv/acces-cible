f953e8c17e7ddd0032a1d824a3ed7fae7ee913a8
Manon Budin <manon.budin@beta.gouv.fr>
2026-07-06 14:26:28 +0200

    chore: Remove inactive scopes and associated recurring jobs
    - Deleted `inactive` scopes from `Team` and `User` models.
    - Removed recurring jobs for cleaning up inactive users and teams.
    
    Signed-off-by: Manon Budin <manon.budin@beta.gouv.fr>

--- app/jobs/remove_inactive_teams_job.rb
+++
@@ -1,7 +0,0 @@
-class RemoveInactiveTeamsJob < ApplicationJob
-  queue_as :default
-
-  def perform
-    Team.inactive.without_users.destroy_all
-  end
-end
--- app/jobs/remove_inactive_users_job.rb
+++
@@ -1,7 +0,0 @@
-class RemoveInactiveUsersJob < ApplicationJob
-  queue_as :default
-
-  def perform
-    User.inactive.destroy_all
-  end
-end
