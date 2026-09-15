7a90ab276a838392f280fdee5f25ea192ac4982a
Manon Budin <manon.budin@beta.gouv.fr>
2026-09-15 07:20:54 +0200

    feat: add displayed columns modal to audits index
    - Front only
    - Patch dsfr_modal to use the official and documented DSFR grid (fr-col-md-10 fr-col-lg-8) https://www.systeme-de-design.gouv.fr/version-courante/fr/composants/modale/demonstration-de-la-modale
    
    Signed-off-by: Manon Budin <manon.budin@beta.gouv.fr>

---
+++ app/components/dsfr/modal_component.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Dsfr
+  class ModalComponent < DsfrComponent::ModalComponent
+  end
+end
--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -3,9 +3,18 @@ class AuditsController < ApplicationController
   include AuditsFiltering
   before_action :set_site, only: [:create, :show]
 
+  COLUMNS = {
+    main_results: { compliance_rate: true, declared_level: true, legal_obligations: true, declaration_quality: true },
+    legal_obligations: { accessibility_page: false, accessibility_mention: false, schema: false, plan: false },
+    declaration_quality: { declaration_date: false, standard: false, auditor: false, law_article: false, contact: false, declaration_format: false, schema_quality: false, plan_quality: false },
+    automated_tests: { automated_tests_result: false, automated_tests_applicable: false, automated_tests_passed: false, automated_tests_inapplicable: false },
+    meta: { reachable: true, evaluator: true, organization_label: true, last_audit_at: true, tags: true }
+  }.freeze
+
   # GET /audits
   def index
     params[:sort] ||= { last_audited_at: AuditsFiltering::DEFAULT_DIRECTION }
+    @columns = COLUMNS
     @tags = current_user.team.tags.in_alphabetical_order
     @pagy, @audits = pagy(scoped_audits.displayable)
   end
